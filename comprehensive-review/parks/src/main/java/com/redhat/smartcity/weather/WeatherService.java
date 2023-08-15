ece857413a7caf64d0b8b86dac652cb0e0e05255
marcioassuncao <foco.marcio.assuncao@gmail.com>
2023-08-14 23:33:12 -0400

    Lab: Red Hat Cloud-native Microservices Development with Quarkus

---
+++ comprehensive-review/parks/src/main/java/com/redhat/smartcity/weather/WeatherService.java
@@ -0,0 +1,20 @@
+package com.redhat.smartcity.weather;
+
+
+
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import io.smallrye.mutiny.Uni;
+
+@Path("/warnings")
+@RegisterRestClient(configKey="weather-api")
+public interface WeatherService {
+
+    @GET
+    @Path("/{city}")
+    Uni<List<WeatherWarning>> getWarningsByCity(@PathParam("city") String city);
+}
